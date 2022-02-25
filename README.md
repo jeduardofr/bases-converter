b6c34aec5956a0e57fc27247f3f278097ba62c13
jeduardofr <jeduardo.fuentes@alumnos.udg.mx>
2022-02-25 00:43:44 -0600

    Program with main loop

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Base Converter
 
-- [ ] Prompt for value which can range from base 2 to 30 
-- [ ] Iterative menu
-- [ ] Specify initial base and final base
+- [x] Prompt for value which can range from base 2 to 30 
+- [x] Iterative menu
+- [x] Specify initial base and final base
 - [x] Validate that the value actually belongs to the initial base
-- [ ] Compute the convertion
-	- [ ] Convert from any base to base 10
-	- [ ] Convert from base 10 to any base
+- [x] Compute the convertion
+	- [x] Convert from any base to base 10
+	- [x] Convert from base 10 to any base
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,51 @@
 #include <converter/validator.h>
+#include <converter/converter.h>
 #include <iostream>
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  int option;
+  char input[80];
+  uint32_t initial_base, final_base;
+  bool exit = false;
+
+  do {
+    std::cout << "1. Calcular número\n";
+    std::cout << "2. Salir\n";
+
+    std::cout << "> ";
+    std::cin >> option;
+
+    switch (option) {
+      case 1: {
+        bool control = false;
+        std::cout << "Ingresa el valor a convertir en un rango de base de 2 a 30\n";
+        std::cout << "> ";
+        std::cin >> input;
+
+        std::cout << "Ingresa la base de entrada\n";
+        std::cout << "> ";
+        std::cin >> initial_base;
+
+        std::cout << "Ingresa la base de salida\n";
+        std::cout << "> ";
+        std::cin >> final_base;
+
+        if (!converter::validate_string(input, initial_base)) {
+          std::cout << "El valor que ingresaste usa caracteres no permitidos por la base de entrada seleccionada\n\n";
+          break;
+        }
+
+        auto result = converter::convert(input, initial_base, final_base);
+        std::cout << "El valor " << input << " de base " << initial_base << " a base " << final_base << " es " << result << "\n\n";
+        delete result;
+      } break;
+      case 2:
+        exit = true;
+        break;
+      default:
+        std::cout << "Opción inválida\n";
+    }
+  } while(exit != true);
+
+  return 0;
+}
